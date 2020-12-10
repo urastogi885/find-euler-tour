b449b8d7147250d4fa3799d1b3705f7ae43148c9
umang <umang.rastogi96@gmail.com>
2020-12-10 16:54:24 -0500

    [euler_tour.c] Finish euler tour implementation

--- euler_tour.c
+++ euler_tour.c
@@ -5,113 +5,170 @@
 
 typedef struct Node 
 {
-    // data of the node
+    // Data of the node
     int num;
-    // address of the next node
+    // Address of the next node
     struct Node *next;
 } Node;
 
-int push_front( Node **head, int data )
+void push_front( Node **head, int data )
 {
     Node *new_node = malloc( sizeof( Node ) );
-    int success = new_node != NULL;
-
-    if ( success )
-    {
-        new_node->num = data;
-        new_node->next = *head;
-        *head = new_node;
-    }
-
-    return success;
+    // Add data to the new node
+    new_node->num = data;
+    // Point next of the node to the original linked-list
+    new_node->next = *head;
+    // Assign head to new node
+    *head = new_node;
 }
 
-void output( Node **head )
-{
-    for( Node *current =*head; current != NULL; current = current->next )
-    {
-        printf( "%d ", current->num );
-    }
-    printf( "%s", "NULL" );
-}
+void delete_node(struct Node **head, int data) 
+{ 
+    // Store head node 
+    struct Node* temp = *head, *prev; 
+  
+    // If head node itself holds the data to be deleted 
+    if (temp != NULL && temp->num == data) 
+    { 
+        *head = temp->next;   // Changed head 
+        free(temp);               // free old head 
+        return; 
+    } 
+  
+    // Search for the data to be deleted, keep track of the 
+    // previous node as we need to change 'prev->next' 
+    while (temp != NULL && temp->num != data) 
+    { 
+        prev = temp; 
+        temp = temp->next; 
+    } 
+  
+    // If data was not present in linked list 
+    if (temp == NULL) return; 
+  
+    // Unlink the node from linked list 
+    prev->next = temp->next; 
 
-void display( Node **set, size_t n )
-{
-    for ( size_t i = 0; i < n; i++ )
-    {
-        output( set++ );
-        putchar( '\n' );
-    }
+    // Free memory 
+    free(temp);
 }
 
 int main( int argc, char *argv[] )
 {
-    // check if graph has been provided by the user as an input
+    // Check if graph has been provided by the user as an input
 	if ( argc < 2 )
 	{
 		printf( "Graph input file not provided!\n" );
 		return 0;
 	}
+
+    // Start timer
     clock_t begin = clock();
 
-    // read input from file
+    // Read input from file
     FILE *ifp;
+    // TODO: May be use a loop to run for all given input files
 	ifp = fopen( argv[1], "r" );
-    // ifp = fopen( "input/input_example.txt", "r" );
 
-    // count the number of vertices in the graph
+    // Count the number of vertices in the graph
     int num_v = 0;
 	char l[256];
 	while ( fgets( l, sizeof l, ifp ) != NULL ) num_v++;
+    // Revert the pointer back to the start of the text file
 	rewind( ifp );
 
-    // create an array of empty linked list to store the adjaccency list of the graph
+    // Create an array of empty linked list to store the adjaccency list of the graph
     Node * adj_list[num_v];
     for ( int i=0; i < num_v; i++ ) adj_list[i] = NULL;
 
-    // create an adjacency list using the input text file
+    // Define variable to check if euler tour exists in the given graph
+    unsigned int check_eulerian = 1;
+
+    // Create an adjacency list using the input text file
     char line[1024];
     unsigned int counter = 0;
-    // define variable to check if euler tour exists in the given graph
-    unsigned int check_eulerian = 1;
+    // Go through each line of the text file
 	while (fgets(line, sizeof line, ifp) != NULL)
 	{
-        int src_vertex, dst_vertex;
-        int degree = 0;
+        unsigned int src_vertex, dst_vertex;
+        unsigned int degree = 0;
 		char * pch;
 		pch = strtok(line, " \n\r");
-        // get source vertex    // TODO: remove the comment below. add it to the report instead 
+        // Get source vertex    // TODO: remove the comment below. add it to the report instead 
         // not storing the source vertex since the index of array will provide us the source veretx
 		sscanf(pch, "%d", &src_vertex);
 		pch = strtok(NULL, " \n\r");
 
-        // get various vertices forming an edge with the source vertex
+        // Get various vertices forming an edge with the source vertex
 		while (pch != NULL)
 		{
 			sscanf(pch, "%d", &dst_vertex);
-            // add vertex to the adjacency list
+            // Add vertex to the adjacency list
             push_front( &adj_list[counter], dst_vertex-1 );
 			pch = strtok(NULL, " \n\r");
             degree++;
 		}
 
-        // check if graph contains an euler tour
-        if ( degree%2 != 0 ) 
+        // Check if graph contains an euler tour
+        if ( degree % 2 != 0 ) 
         {
             check_eulerian = 0;
             break;
         }
 
-        // increment counter to enter vertices in the next linked list
+        // Increment counter to enter vertices in the next linked list
         counter++;
 	}
-    // display( adj_list, sizeof( adj_list )/sizeof( *adj_list ) );
+
+    // Close the text file since adjacency list has been created
     fclose( ifp );
 
+    // Check if an Euler tour exists in the graph
     if ( !check_eulerian )
     {
+        // TODO: Generate appropriate output file instead
         printf("The given graph does not contain an Euler tour!\n");
         return 0;
     }
+
+    // Use the first vertex as the starting point
+    unsigned int v = 0;
+    unsigned int w = 0;
+
+    // Initialize a linked-list to store the euler tour
+    Node *euler_tour = malloc( sizeof( Node ) );
+    euler_tour->num = v + 1;
+    euler_tour->next = NULL;
+
+    // Go through all the edges to find an euler tour
+    while ( adj_list[v] != NULL )
+    {
+        // Get the other vertex corresponding to the edge v and head of linked-list associated with v
+        w = adj_list[v]->num;
+
+        // Delete edge (v,w) and (w,v)
+        delete_node( &adj_list[v], w );
+        delete_node( &adj_list[w], v );
+
+        // Now that we have traveled (v,w), look for next edge to travel from w
+        v = w;
+
+        // Add this edge to the Euler tour
+        push_front( &euler_tour, v + 1);
+    }
+
+    // Print the Euler tour
+    // TODO: store this in a text file instead
+    while (euler_tour != NULL) 
+    { 
+        printf(" %d ", euler_tour->num); 
+        euler_tour = euler_tour->next;
+    }
+    printf( "\n" );
+
+    // Print overall time taken in seconds
     printf("Time taken = %lf\n", ((double)clock()-begin)/CLOCKS_PER_SEC);
+
+    // Indicate successful execution for finding an euler tour in the input graph
+    return 1;
 }
