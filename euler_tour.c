17f528e5a0e12956d6d094177627aa2c4100fc4d
umang <umang.rastogi96@gmail.com>
2020-12-12 12:33:08 -0500

    [euler_tour.c] Add counting for no. of commands

--- euler_tour.c
+++ euler_tour.c
@@ -26,39 +26,45 @@ void push_front( Node **head, int data )
     *head = new_node;
 }
 
-void delete_node(struct Node **head, int data) 
+void delete_node( struct Node **head, int data ) 
 { 
     // Store head node 
     struct Node* temp = *head, *prev; 
   
     // If head node itself holds the data to be deleted 
-    if (temp != NULL && temp->num == data) 
-    { 
-        *head = temp->next;   // Changed head 
-        free(temp);               // free old head 
+    if ( temp != NULL && temp->num == data ) 
+    {
+        // Change head
+        *head = temp->next;
+        // Free old head
+        free( temp );
         return; 
     } 
   
     // Search for the data to be deleted, keep track of the 
     // previous node as we need to change 'prev->next' 
-    while (temp != NULL && temp->num != data) 
+    while ( temp != NULL && temp->num != data ) 
     { 
         prev = temp; 
         temp = temp->next; 
-    } 
+    }
   
     // If data was not present in linked list 
-    if (temp == NULL) return; 
+    if ( temp == NULL ) return; 
   
     // Unlink the node from linked list 
     prev->next = temp->next; 
 
     // Free memory 
-    free(temp);
+    free( temp );
 }
 
 int main( int argc, char *argv[] )
 {
+    // Initialize variable to count all the operations
+    // Note that any operations pertaining to the counting the operations have been excluded from the count
+    unsigned int count_operations = 0;
+
     // Check if graph has been provided by the user as an input
 	if ( argc < 2 )
 	{
@@ -69,9 +75,8 @@ int main( int argc, char *argv[] )
     // Start timer
     clock_t begin = clock();
 
-    // Read input from file
+    // Open input text file for reading
     FILE *ifp;
-    // TODO: May be use a loop to run for all given input files
 	ifp = fopen( argv[1], "r" );
 
     // Count the number of vertices in the graph
@@ -91,37 +96,53 @@ int main( int argc, char *argv[] )
     // Create an adjacency list using the input text file
     char line[1024];
     unsigned int counter = 0;
-    // Go through each line of the text file
-	while (fgets(line, sizeof line, ifp) != NULL)
+    unsigned int num_edges = 0;
+
+    // Increment operations count
+    count_operations += ( 2 * num_v ) + 10;
+
+    // Go through each line of the text file to create the adjacency list
+	while ( fgets( line, sizeof line, ifp ) != NULL )
 	{
+        // Initialize source and destination vertices to represent an edge
         unsigned int src_vertex, dst_vertex;
+        // Initialize degree of each vertex as 0
         unsigned int degree = 0;
+        // Initialize finder to search for vertices in the text
 		char * pch;
-		pch = strtok(line, " \n\r");
-        // Get source vertex    // TODO: remove the comment below. add it to the report instead 
-        // not storing the source vertex since the index of array will provide us the source veretx
-		sscanf(pch, "%d", &src_vertex);
-		pch = strtok(NULL, " \n\r");
+		pch = strtok( line, " \n\r" );
+        // Find the source vertex
+        // Not storing the source vertex in the adjacency since the index of array will provide us the source veretx
+		sscanf( pch, "%d", &src_vertex );
+		pch = strtok( NULL, " \n\r" );
 
         // Get various vertices forming an edge with the source vertex
-		while (pch != NULL)
+		while ( pch != NULL )
 		{
-			sscanf(pch, "%d", &dst_vertex);
+            // Find next destination vertex
+			sscanf( pch, "%d", &dst_vertex );
             // Add vertex to the adjacency list
             push_front( &adj_list[counter], dst_vertex-1 );
-			pch = strtok(NULL, " \n\r");
+			pch = strtok( NULL, " \n\r" );
             degree++;
+            num_edges++;
+            // Increment operations count for the above commands
+            count_operations += 5;
 		}
 
         // Check if graph contains an euler tour
         if ( degree % 2 != 0 ) 
         {
+            // Increment operations count by 2 for the next commands
+            count_operations += 2;
             check_eulerian = 0;
             break;
         }
 
         // Increment counter to enter vertices in the next linked list
         counter++;
+        // Increment operations count for the above commands
+        count_operations += 8;
 	}
 
     // Close the text file since adjacency list has been created
@@ -129,14 +150,30 @@ int main( int argc, char *argv[] )
 
     // Open the output file and add whether an Euler tour exists for the graph 
     ifp = fopen( EULER_TOUR_FILE, "w+" );
-    fprintf( ifp, "%d\n", check_eulerian);
+    fprintf( ifp, "%d\n", check_eulerian );
+
+    // Increment operations count for the above commands (outside of the while loop)
+    count_operations += 3;
 
     // Check if an Euler tour exists in the graph
     if ( !check_eulerian )
     {
-        printf("The given graph does not contain an Euler tour!\n");
-        // Close the output file
+        // Increment operations count for the next commands and print it on the terminal
+        count_operations += 6;
+        printf( "Total operations: %d\n", count_operations );
+
+        // Store total operations in another text file
+        FILE *ofp;
+        ofp = fopen( OPERATIONS_NUM_FILE, "w+" );
+        fprintf( ofp, "Total number of operations is: %d\n", count_operations );
+        fclose( ofp );
+
+        // Close the output text file that stores the Euler tour if it exists
         fclose( ifp );
+
+        printf( "The given graph does not contain an Euler tour!\n" );
+        // Print overall time taken in seconds
+        printf( "Time taken = %lf\n", ((double)clock()-begin)/CLOCKS_PER_SEC );
         return 0;
     }
 
@@ -163,23 +200,33 @@ int main( int argc, char *argv[] )
         v = w;
 
         // Add this edge to the Euler tour
-        push_front( &euler_tour, v + 1);
+        push_front( &euler_tour, v + 1 );
     }
 
-    // Print the Euler tour
-    // TODO: store this in a text file
-    while (euler_tour != NULL) 
+    // Increment operations count for the above commands and loop to find the Euler tour
+    count_operations += ( 5 * num_edges ) + 5;
+
+    // Store the Euler tour as output in a text file
+    while ( euler_tour != NULL ) 
     {
-        printf( " %d ", euler_tour->num );
-        fprintf( ifp, "%d ", euler_tour->num);
+        fprintf( ifp, "%d ", euler_tour->num );
         euler_tour = euler_tour->next;
+        // Increment operations count for the above commands
+        count_operations += 2;
     }
-    printf( "\n" );
 
+    // Close the output text file that stores the Euler tour if it exists
     fclose( ifp );
 
+    // Increment operations count for the next commands, store it in an output text file, and print it on the terminal
+    count_operations += 5;
+    ifp = fopen( OPERATIONS_NUM_FILE, "w+" );
+    fprintf( ifp, "Total number of operations is: %d\n", count_operations );
+    fclose( ifp );
+    printf( "Total operations: %d\n", count_operations );
+
     // Print overall time taken in seconds
-    printf("Time taken = %lf\n", ((double)clock()-begin)/CLOCKS_PER_SEC);
+    printf( "Time taken = %lf\n", ((double)clock()-begin)/CLOCKS_PER_SEC );
 
     // Indicate successful execution for finding an euler tour in the input graph
     return 1;
